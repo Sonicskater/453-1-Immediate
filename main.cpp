a111f806921c3cd099cbdf575f22f255bda1d6c7
Devon Hockley <devon@hockley.ca>
2019-10-02 20:03:48 -0600

    thanks i hate it

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@
 #include "glad/glad.h"
 
 //
-#define GLFW_INCLUDE_GLU 
+
 #include "GLFW/glfw3.h"
+#include <cmath>
+//#include "common_matrices.cpp"
+
 
 #define RAD 57.29577951
 #define DegToRad(angle)	((angle)/RAD)
@@ -22,7 +25,6 @@ float green[] = { 0.0f, 1.0f, 0.0f };
 float red[] = { 1.0f, 0.0f, 0.0f };
 float orange[] = { 1.0f, 0.5f, 0.0f };
 float yellow[] = { 1.0f, 1.0f, 0.0f };
-
 struct V3f	// declaration and initializatoin of the "V3f" structure
 {
 	float x, y, z;
@@ -133,63 +135,7 @@ void drawTri(Triangle t) {
 	glVertex2f(t[1].x, t[1].y);
 	glVertex2f(t[2].x, t[2].y);
 }
-void drawCubeSimple() {
-	glBegin(GL_QUADS);
 
-	// Top face
-	glColor3f(0.0f, 1.0f, 0.0f);  // Green
-	glVertex3f(1.0f, 1.0f, -1.0f);  // Top-right of top face
-	glVertex3f(-1.0f, 1.0f, -1.0f);  // Top-left of top face
-	glVertex3f(-1.0f, 1.0f, 1.0f);  // Bottom-left of top face
-	glVertex3f(1.0f, 1.0f, 1.0f);  // Bottom-right of top face
-
-	// Bottom face
-	glColor3f(1.0f, 0.5f, 0.0f); // Orange
-	glVertex3f(1.0f, -1.0f, -1.0f); // Top-right of bottom face
-	glVertex3f(-1.0f, -1.0f, -1.0f); // Top-left of bottom face
-	glVertex3f(-1.0f, -1.0f, 1.0f); // Bottom-left of bottom face
-	glVertex3f(1.0f, -1.0f, 1.0f); // Bottom-right of bottom face
-
-	// Front face
-	glColor3f(1.0f, 0.0f, 0.0f);  // Red
-	glVertex3f(1.0f, 1.0f, 1.0f);  // Top-Right of front face
-	glVertex3f(-1.0f, 1.0f, 1.0f);  // Top-left of front face
-	glVertex3f(-1.0f, -1.0f, 1.0f);  // Bottom-left of front face
-	glVertex3f(1.0f, -1.0f, 1.0f);  // Bottom-right of front face
-
-	// Back face
-	glColor3f(1.0f, 1.0f, 0.0f); // Yellow
-	glVertex3f(1.0f, -1.0f, -1.0f); // Bottom-Left of back face
-	glVertex3f(-1.0f, -1.0f, -1.0f); // Bottom-Right of back face
-	glVertex3f(-1.0f, 1.0f, -1.0f); // Top-Right of back face
-	glVertex3f(1.0f, 1.0f, -1.0f); // Top-Left of back face
-
-	// Left face
-	glColor3f(0.0f, 0.0f, 1.0f);  // Blue
-	glVertex3f(-1.0f, 1.0f, 1.0f);  // Top-Right of left face
-	glVertex3f(-1.0f, 1.0f, -1.0f);  // Top-Left of left face
-	glVertex3f(-1.0f, -1.0f, -1.0f);  // Bottom-Left of left face
-	glVertex3f(-1.0f, -1.0f, 1.0f);  // Bottom-Right of left face
-
-	// Right face
-	glColor3f(1.0f, 0.0f, 1.0f);  // Violet
-	glVertex3f(1.0f, 1.0f, 1.0f);  // Top-Right of left face
-	glVertex3f(1.0f, 1.0f, -1.0f);  // Top-Left of left face
-	glVertex3f(1.0f, -1.0f, -1.0f);  // Bottom-Left of left face
-	glVertex3f(1.0f, -1.0f, 1.0f);  // Bottom-Right of left face
-	glEnd();
-}
-void drawFace(GLdouble topLeft[], GLdouble topRight[], GLdouble bottomRight[], GLdouble bottomLeft[], GLfloat color[]){
-
-	glColor3fv(color);
-
-	glVertex3dv(topRight);
-	glVertex3dv(topLeft);
-	glVertex3dv(bottomLeft);
-	glVertex3dv(bottomRight);
-
-
-}
 void drawFaceTris(GLdouble topLeft[], GLdouble topRight[], GLdouble bottomRight[], GLdouble bottomLeft[], GLfloat color[], std::vector<Triangle> &tris) {
 
 
@@ -197,46 +143,6 @@ void drawFaceTris(GLdouble topLeft[], GLdouble topRight[], GLdouble bottomRight[
 	Triangle br = Triangle(topRight, bottomLeft, bottomRight, color);
 	tris.push_back(tl);
 	tris.push_back(br);
-
-}
-
-
-
-
-
-void drawCube(double x, double y, double z, double size, std::vector<bool> faces) {
-	GLdouble tfl[] = { x,y,z };
-	GLdouble tfr[] = { x+size,y,z };
-	
-	GLdouble tbl[] = { x,y,z-size };
-	GLdouble tbr[] = { x+size,y,z-size };
-
-	GLdouble bfl[] = { x,y-size,z };
-	GLdouble bfr[] = { x+size,y-size,z };
-
-	GLdouble bbl[] = { x,y-size,z-size };
-	GLdouble bbr[] = { x+size,y-size,z-size };
-
-
-	
-
-	//top face
-	if (faces[0]) drawFace(tbl, tbr, tfr, tfl, green);
-
-	//bottom
-	if (faces[1]) drawFace(bbr, bbl, bfl, bfr, orange);
-
-	//front face
-	if (faces[2]) drawFace(tfl, tfr, bfr, bfl, red);
-
-	//back face
-	if (faces[3]) drawFace(tbr, tbl, bbl, bbr, yellow);
-
-	//left face
-	if (faces[4]) drawFace(tbl, tfl, bfl,bbl,blue);
-
-	//right face
-	if (faces[5]) drawFace(tfr, tbr, bbr, bfr, violet);
 
 }
 
@@ -277,69 +183,6 @@ void drawCubeTris(double x, double y, double z, double size, std::vector<bool> f
 
 }
 
-void drawSponge(double x, double y, double z, double size, int r, std::vector<bool> faces) {
-	if (!r) {
-		drawCube(x, y, z, size,faces);
-	}
-	else {
-		r = r - 1;
-		double s = size / 3;
-
-		double layer_depth = z;
-		//front cubes
-		
-			// top layer
-			double up = y;
-			drawSponge(x, up, layer_depth, s, r, faces);
-			drawSponge(x+s, up, layer_depth, s, r, faces);
-			drawSponge(x+2*s, up, layer_depth, s, r, faces);
-
-			
-			// middle layer
-			up = y - s;
-			drawSponge(x+2*s, up, layer_depth, s, r, faces);
-			drawSponge(x, up, layer_depth, s, r, faces);
-
-			// bottom layer
-			up = y - 2 * s;
-			drawSponge(x, up, layer_depth, s, r, faces);
-			drawSponge(x+s, up, layer_depth, s, r, faces);
-			drawSponge(x+2*s, up, layer_depth, s, r, faces);
-
-		//connectors
-		layer_depth = z - s;
-
-			// top
-			up = y;
-			drawSponge(x, up, layer_depth, s, r, faces);
-			drawSponge(x+2*s, up, layer_depth, s, r, faces);
-
-			// bottom
-			up = y - 2 * s;
-			drawSponge(x, up, layer_depth, s, r, faces);
-			drawSponge(x+2*s, up, layer_depth, s, r, faces);
-
-		//back cubes
-		layer_depth = z - 2 * s;
-
-			// top layer
-			up = y;
-			drawSponge(x, up, layer_depth, s, r, faces);
-			drawSponge(x+s, up, layer_depth, s, r, faces);
-			drawSponge(x+2*s, up, layer_depth, s, r, faces);
-
-			// mid layer
-			up = y - s;
-			drawSponge(x+2*s, up, layer_depth, s, r, faces);
-			drawSponge(x, up, layer_depth, s, r, faces);
-
-			// bottom layer
-			up = y - 2 * s;
-			drawSponge(x, up, layer_depth, s, r, faces);
-			drawSponge(x + s, up, layer_depth, s, r, faces);
-			drawSponge(x + 2 * s, up, layer_depth, s, r, faces);
-	}
-}
 
 void drawSpongeTris(double x, double y, double z, double size, int r, std::vector<bool> faces, std::vector<Triangle> &tris) {
 	if (!r) {
@@ -405,103 +248,6 @@ void drawSpongeTris(double x, double y, double z, double size, int r, std::vecto
 	}
 }
 
-void drawSpongeCulled(double x, double y, double z, double size, int r, std::vector<bool> faces) {
-	if (!r) {
-		drawCube(x, y, z, size, faces);
-	}
-	else {
-
-		r = r - 1;
-		double s = size / 3;
-		double layer_depth = z;
-		//front cubes
-
-			// top layer
-			double up = y;
-			std::vector<bool> f1 = {faces[0]&&true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f1);
-			std::vector<bool> f2 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + s, up, layer_depth, s, r, f2);
-			std::vector<bool> f3 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f3);
-
-
-			// middle layer
-			up = y - s;
-			std::vector<bool> f4 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f4);
-			std::vector<bool> f5 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f5);
-
-			// bottom layer
-			up = y - 2 * s;
-			std::vector<bool> f6 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f6);
-			std::vector<bool> f7 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + s, up, layer_depth, s, r, f7);
-			std::vector<bool> f8 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f8);
-
-		//connectors
-		layer_depth = z - s;
-
-			// top
-			up = y;
-
-			std::vector<bool> f10 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f10);
-			
-
-			std::vector<bool> f11 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f11);
-
-			// bottom
-			up = y - 2 * s;
-
-			std::vector<bool> f12 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f12);
-
-			std::vector<bool> f13 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f13);
-
-		//back cubes
-		layer_depth = z - 2 * s;
-
-			// top layer
-			up = y;
-
-			std::vector<bool> f14 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f14);
-
-			std::vector<bool> f15 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + s, up, layer_depth, s, r, f15);
-
-			std::vector<bool> f16 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f16);
-
-			// mid layer
-			up = y - s;
-
-			std::vector<bool> f17 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f17);
-
-			std::vector<bool> f18 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f18);
-
-			// bottom layer
-			up = y - 2 * s;
-
-			std::vector<bool> f19 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x, up, layer_depth, s, r, f19);
-
-			std::vector<bool> f20 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + s, up, layer_depth, s, r, f20);
-
-			std::vector<bool> f21 = { faces[0] && true, faces[1] && true, faces[2] && true, faces[3] && true, faces[4] && true, faces[5] && true };
-			drawSpongeCulled(x + 2 * s, up, layer_depth, s, r, f21);
-	}
-}
-
 int recursionLevel = 0;
 bool updateMesh = true;
 float spin = 1.0f;
@@ -509,6 +255,7 @@ float spin2 = 1.0f;
 
 float scale = 1.0f;
 
+bool per = false;
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	if (key == GLFW_KEY_E && action == GLFW_PRESS && recursionLevel <=3) {
@@ -520,16 +267,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		updateMesh = true;
 	}
 	else if (key == GLFW_KEY_W && action == GLFW_PRESS ) {
-		spin--;
-	}
-	else if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
-		spin++;
-	}
-	else if (key == GLFW_KEY_S && action == GLFW_PRESS) {
-		spin2--;
-	}
-	else if (key == GLFW_KEY_X && action == GLFW_PRESS) {
-		spin2++;
+		per = !per;
 	}
 	else if (key == GLFW_KEY_A && action == GLFW_PRESS) {
 		scale+=0.1f;
@@ -570,31 +308,137 @@ void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 int m_width = 800;
 int m_height = 800;
 bool updateWindow = true;
+void draw(std::vector<Triangle> tris);
+
+GLfloat* symmetricFrustumProjectionGl(float r, float t, float n, float f) {
+	float a00 = n / r;
+	float a11 = n / t;
+	float a22 = -(f + n) / (f - n);
+	float a23 = -2.f * f * n / (f - n);
+
+	GLfloat symmetricFrustum[] = {
+		a00, 0.f, 0.f,  0.f, // 0 1 2 3
+		0.f, a11, 0.f,  0.f, // 4 5 6 7
+		0.f, 0.f, a22,  a23, // 8 9 10 11
+		0.f, 0.f, -1.f, 0.f  // 12 13 14 15
+	};
+	return symmetricFrustum;
+}
+GLfloat* perspectiveProjectionGl(float fovDegrees, float aspectRatio, float zNear,
+	float zFar) {
+	float top = std::tan(fovDegrees * (M_PI / 180.f) * (1.f / 2.f)) * zNear;
+	float right = top * aspectRatio;
+
+	return symmetricFrustumProjectionGl(right, top, zNear, zFar);
+}
+std::vector<Triangle> triangles = {};
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	m_width = width;
 	m_height = height;
 	updateWindow = true;
+	glViewport(0, 0, m_width, m_height);
+	draw(triangles);
+}
+
+void setupWindow(GLFWwindow *window) {
+	//Set context to window
+	glfwMakeContextCurrent(window);
+
+	//Load up 
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	glfwSwapInterval(1); // vsync
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+GLFWwindow* window;
+void printGlVersion() {
+	int glfwMajor, glfwMinor, glfwRevision;
+	glfwGetVersion(&glfwMajor, &glfwMinor, &glfwRevision);
+	std::cout << "GLFW Version: " << glfwMajor << "." << glfwMinor << "." << glfwRevision << std::endl;
+}
+
+void glSetup() {
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
+	glDepthFunc(GL_LESS);
+}
+
+void glPerspective(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar)
+{
+	const GLdouble pi = 3.1415926535897932384626433832795;
+	GLdouble fW;
+	GLdouble fH;
+
+	//fH = tan( (fovY / 2) / 180 * pi ) * zNear;
+	fH = tan(fovY / 360 * pi) * zNear;
+	fW = fH * aspect;
+
+	glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+}
+
+void applyTransform() {
+
+
+	glMatrixMode(GL_MODELVIEW);
+	//glPopMatrix();
+	//glPushMatrix();
+	glLoadIdentity();
+	if (per) {
+		glTranslatef(0, 0, -2);
+	}
+	glRotatef(spin, 0, 1.0, 0);
+	glRotatef(spin2, 1, 0, 0);
+	glScaled(scale, scale, scale);
+	glMatrixMode(GL_PROJECTION);
+	auto d = perspectiveProjectionGl(90, m_width / m_height, 0.1, 15);
+	//const GLfloat* g = d;
+	//glPopMatrix();
+	//glPushMatrix();
+	glLoadIdentity();
+	if (per) {
+		glPerspective(90, m_width / m_height, 0.1, 5);
+	}
+	//draw(triangles);
+	glMatrixMode(GL_MODELVIEW);
+	
+	
+}
+
+void draw(std::vector<Triangle> tris) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+	for (auto t : tris) {
+
+		glColor3d(t.m_color.x, t.m_color.y, t.m_color.z);
+			glVertex3d(t[0].x, t[0].y, t[0].z);
+			glVertex3d(t[1].x, t[1].y, t[1].z);
+			glVertex3d(t[2].x, t[2].y, t[2].z);
+		
+	}
+	glEnd();
+
+
+	//Swap current scene with next scene
+	glfwSwapBuffers(window);
+
 }
 
 
-
-
-
 int main() {
-  GLFWwindow *window = nullptr;
+   window = nullptr;
 
   // Close if OpenGL is not initialized
   if (!glfwInit()) {
     exit(EXIT_FAILURE);
   }
-
-
   // Currently installed version of OpenGL
-  int glfwMajor, glfwMinor, glfwRevision;
-  glfwGetVersion(&glfwMajor, &glfwMinor, &glfwRevision);
-  std::cout << "GLFW Version: " << glfwMajor << "." << glfwMinor << "."
-       << glfwRevision << std::endl;
+  printGlVersion();
 
   //Assign OpenGL version for program
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
@@ -606,77 +450,32 @@ int main() {
     glfwTerminate();
     exit(EXIT_FAILURE);
   }
-
-  //Set context to window
-  glfwMakeContextCurrent(window);
   
-  //Load up 
-  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  setupWindow(window);
 
-  glfwSwapInterval(1); // vsync
-  
-  
+  glSetup();
 
-  glTranslatef(0, 0, 0);
-  //
   std::vector<bool> faces = { true, true, true, true, true, true };
-  std::vector<Triangle> tris = {};
-  //drawSpongeTris(-2.5, 2.5, 2.5, 5, recursionLevel, faces, tris);
-  std::cout << tris.size() << "\n";
+
+
   // Rendering loop
-  glfwSetKeyCallback(window, key_callback);
-  glfwSetCursorPosCallback(window, cursor_position_callback);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   while (!glfwWindowShouldClose(window)) {
 
 	  if (updateMesh) {
-		  tris = {};
-		  drawSpongeTris(-0.5, 0.5, 0.5, 1, recursionLevel, faces, tris);
+		  triangles = {};
+		  drawSpongeTris(-0.5, 0.5, 0.5, 1, recursionLevel, faces, triangles);
 		  updateMesh = false;
 	  }
-	  //spin = spin - .5; // inc for spin
-	  if (spin < 360){
-		  //spin = spin + 360;
-	  }
 	  if (updateWindow) {
-		  glViewport(0, 0, m_width, m_height);
+
 		  updateWindow = false;
 	  }
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glCullFace(GL_FRONT);
-	glDepthFunc(GL_LESS);
-	//glu::gluLookAt(0,0,-5,0,0,0,0,1,0);
-	glBegin(GL_TRIANGLES);
-	
-    for (auto t : tris) {
-		
-		glColor3d(t.m_color.x, t.m_color.y, t.m_color.z);
-		  glVertex3d(t[0].x, t[0].y, t[0].z);
-		  glVertex3d(t[1].x, t[1].y, t[1].z);
-		  glVertex3d(t[2].x, t[2].y, t[2].z);
-    }
-	
-	//glBegin(GL_QUADS);
+	  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	  draw(triangles);
+	  applyTransform();
 
 
-	//drawSponge(-2.5, 2.5, 2.5, 5, 5, faces);
-	glEnd();
-
-	glMatrixMode(GL_MODELVIEW);
-	glPopMatrix();
-	glPushMatrix();
-	//glRotatef(90, 1, 0, 0.00);
-	glRotatef(spin, 0,1.0,0);
-	glRotatef(spin2, 1, 0, 0);
-	glScaled(scale, scale, scale);
-	//glRotatef(spin3, 0, 0, 1);
-	//Swap current scene with next scene
-    glfwSwapBuffers(window);
-
-    glfwPollEvents(); // will process event queue and carry on
+      glfwPollEvents(); // will process event queue and carry on
   }
 
   glfwDestroyWindow(window);
